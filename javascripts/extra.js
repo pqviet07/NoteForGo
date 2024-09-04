ad9bcb9388fb4cd31814ba36544e07e0792379d1
pqviet07 <phungquocviet07@gmail.com>
2024-09-04 23:11:41 +0700

    First commit

---
+++ javascripts/extra.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Select the button by its classes
+    const subscribeButton = document.querySelector('.md-button.md-button--primary');
+
+    // Check if the button exists
+    if (subscribeButton) {
+        // Attach a click event listener
+        subscribeButton.addEventListener('click', function (event) {
+            event.preventDefault(); // Prevent the default link behavior
+            alert('Thank you for subscribing!');
+            // You can add more custom behavior here
+        });
+    }
+
+
+    // Select all elements with the .md-clipboard class
+    const clickedTargets = document.querySelectorAll('.md-clipboard');
+    clickedTargets.forEach(function (clickedTarget) {
+        clickedTarget.addEventListener('click', function (e) {
+            confetti({
+                angle: randomInRange(55, 125),
+                spread: randomInRange(50, 70),
+                particleCount: randomInRange(50, 100),
+                origin: { y: 0.6 },
+            });
+
+        });
+    });
+
+    function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+});
